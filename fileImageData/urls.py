749344f3495a8a8f6882935a795730d6307738e7
Nodar Svanidze <99223872+nodarisvanidze88@users.noreply.github.com>
2023-09-15 05:48:25 +0400

    add new url for customers

--- fileImageData/urls.py
+++ fileImageData/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import getCSVFile, getItemsList
-from django.views import View
+from .views import getCSVFile, getItemsList, getWithoutImage, getUsers
+
 
 
 urlpatterns = [
     path('test', getCSVFile, name="csvFile"),
-    path('allItems', getItemsList, name='itemlist')
+    path('allItems', getItemsList, name='itemlist'),
+    path('withoutimages', getWithoutImage, name='withoutImages'),
+    path('getusers', getUsers, name="getusers"),
 ]
